4795db01b42bd4e08afd967033cf29b48f54fc24
Mauricio <marogonza2003@gmail.com>
2024-09-03 23:04:06 -0500

    Creando microservicio de capacidades y importando las librerias necesarias.

---
+++ microservicio-capacidad/microservicio-capacidad/src/main/java/com/example/microservicio_capacidad/MicroservicioCapacidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_capacidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCapacidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCapacidadApplication.class, args);
+	}
+
+}
---
+++ microservicio-capacidad/microservicio-capacidad/src/test/java/com/example/microservicio_capacidad/MicroservicioCapacidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_capacidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCapacidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
